bc1a94908328dd4c1ed149eefb77efc9ca7f02ba
olirumtecapp-debug <olirumtecapp@gmail.com>
2026-08-22 07:39:01 -0300

    feat: limpeza de botoes de apoio e app, versao standalone pronta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { MuriloMasterAdmin } from "@/components/MuriloMasterAdmin";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   Outlet,
@@ -119,7 +120,8 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
+      {/* Required: nested routes render here. Removing <Outlet />
+      <MuriloMasterAdmin /> breaks all child routes. */}
       <Outlet />
       <Toaster theme="dark" position="top-center" richColors />
     </QueryClientProvider>
